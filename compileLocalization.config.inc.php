b41ad04c3b1dc851e4c36e00abb21e9ead87880c
Daniel Popiniuc <danielpopiniuc@gmail.com>
2014-12-07 22:09:39 +0200

    re-located the compiler location used by me

--- compileLocalization.config.inc.php
+++ compileLocalization.config.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-define('APP_INDICATIVE', 'execPOcompilation');
-define('GETTEXT_COMPILER', 'D:\\www\\App\\GetText\\msgfmt.exe');
+define('GETTEXT_COMPILER', 'D:\\www\\AppForDeveloper\\GetText\\msgfmt.exe');
 define('ERROR_DIR', pathinfo(ini_get('error_log'))['dirname']);
 define('ERROR_FILE', 'php' . PHP_VERSION_ID . 'errors_ExecPoCompilation_' . date('Y-m-d') . '.log');
